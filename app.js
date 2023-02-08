3043b594911897575a8cab4ff3f6b290116501f1
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-08 17:58:49 +0100

    iteration 5 & 6 - done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -24,8 +25,30 @@ app.get('/beers', (req, res) => {
   punkAPI
   .getBeers()
   .then(beersFromApi => {
-    res.locals.beers = beersFromApi
+    res.locals.beers = []
+    beersFromApi.forEach(element => {
+      res.locals.beers.push({
+        id: element.id,
+        image_url: element.image_url,
+        name: element.name,
+        description: element.description,
+        tagline: element.tagline,
+      })
+    });
     res.render("beers")
+  })
+  .catch(error => {
+    res.statusCode = 500
+    res.render("error", {code:500})
+  });
+})
+
+app.get("/beer-:id", (req, res) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then(responseFromAPI => {
+    res.locals.beer = responseFromAPI[0]
+    res.render("random-beer")
   })
   .catch(error => {
     res.statusCode = 500
